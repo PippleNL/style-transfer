2a4fd9e44815c7e31ed201237a65c451961153f6
RobinSwinkelsPipple <robin@pipple.nl>
2020-09-18 11:51:09 +0200

    async working model

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,4 @@
-# backend/main.py
-
 import uuid
-
 import cv2
 import uvicorn
 from fastapi import File
@@ -12,7 +9,12 @@ from PIL import Image
 
 import config
 import inference
+import asyncio
+import time
 
+from concurrent.futures import ProcessPoolExecutor
+
+from functools import partial
 
 app = FastAPI()
 
@@ -23,13 +25,33 @@ def read_root():
 
 
 @app.post("/{style}")
-def get_image(style: str, file: UploadFile = File(...)):
+async def get_image(style: str, file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
     model = config.STYLES[style]
+    start = time.time()
     output, resized = inference.inference(model, image)
     name = f"/storage/{str(uuid.uuid4())}.jpg"
     cv2.imwrite(name, output)
-    return {"name": name}
+    models = config.STYLES.copy()
+    del models[style]
+    asyncio.create_task(generate_remaining_models(models, image, name))
+    return {"name": name, "time": time.time() - start}
+
+
+async def generate_remaining_models(models, image, name: str):
+    executor = ProcessPoolExecutor()
+    event_loop = asyncio.get_event_loop()
+    await event_loop.run_in_executor(
+        executor, partial(process_image, models, image, name)
+    )
+
+
+def process_image(models, image, name: str):
+    for model in models:
+        output, resized = inference.inference(models[model], image)
+        name = name.split(".")[0]
+        name = f"{name.split('_')[0]}_{models[model]}.jpg"
+        cv2.imwrite(name, output)
 
 
 if __name__ == "__main__":
